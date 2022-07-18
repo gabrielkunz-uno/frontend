2985e445a834a4365248055a1bdb679cea01408b
gabrielkunz-uno <gabriel.kunz@unochapeco.edu.br>
2022-07-18 17:47:57 -0300

    ajustados os warnings

--- pages/todo.vue
+++ pages/todo.vue
@@ -3,29 +3,31 @@
     <v-row>
       <v-col>
         <v-text-field
-          v-model='descricao'
+          v-model="descricao"
           outlined
-          placeholder='Informe o TODO'
-          label='Descrição do TODO'
+          placeholder="Informe o TODO"
+          label="Descrição do TODO"
           @keypress="handleKeyPress"
-        ></v-text-field>
+        />
       </v-col>
       <v-col>
-        <v-btn @click='cadastrar' class='mx-2' fab dark color='indigo'>
-          <v-icon dark> mdi-plus </v-icon>
+        <v-btn class="mx-2" fab dark color="indigo" @click="cadastrar">
+          <v-icon dark>
+            mdi-plus
+          </v-icon>
         </v-btn>
       </v-col>
     </v-row>
     <v-container>
       <v-row
-        v-for='(todo, i) in todos'
-        :key='i'
+        v-for="(todo, i) in todos"
+        :key="i"
       >
         <h3>{{ todo }}</h3>
         <v-btn
-          @click='excluir(i)'
           x-small
-          style='margin-left: 10px; margin-top: 5px;'
+          style="margin-left: 10px; margin-top: 5px;"
+          @click="excluir(i)"
         >
           Excluir
         </v-btn>
@@ -58,7 +60,6 @@ export default {
     },
 
     handleKeyPress (evento) {
-      console.log(evento)
       if (evento.keyCode === 13) {
         this.cadastrar()
       }
